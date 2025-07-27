500b1e98f6a414ef9baa4d6c3ac7966c8fbcb72b
vasundhara_panwar <123188761+2627Prash@users.noreply.github.com>
2025-07-27 15:52:59 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull admin2627/python-app:latest
+docker pull admin2627/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 admin2627/python-app:latest
+docker run -d -p 5000:5000 admin2627/simple-python-app:latest
